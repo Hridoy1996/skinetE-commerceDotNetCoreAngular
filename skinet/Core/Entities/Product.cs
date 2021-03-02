def442474ad6ea928e8b85d45814d17eb4cf4feb
hridoy <hridoytareqmahmud@gmail.com>
2021-03-02 09:46:52 +0600

    hmm

--- skinet/Core/Entities/Product.cs
+++ skinet/Core/Entities/Product.cs
@@ -4,9 +4,16 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace Core.Entities
 {
   [BsonIgnoreExtraElements]
-  public class Product
+  public class Product: BaseEntity
   {
-    public string Id {get; set;}
-    public string Name {get; set;}
-  }
+        public string Name {get; set;}
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public ProductType ProductType { get; set; }
+        public string ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public string ProductBrandId { get; set; }
+
+    }
 }
