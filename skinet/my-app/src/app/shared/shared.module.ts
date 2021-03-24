57102e580349a1fceed15c8fa6a5731b5d9dd4fc
hridoy <hridoytareqmahmud@gmail.com>
2021-03-24 22:46:58 +0600

    update gitignore

--- skinet/my-app/src/app/shared/shared.module.ts
+++ skinet/my-app/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PaginationModule } from 'ngx-bootstrap';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PagingHeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PaginationModule.foeRoot()
+  ],
+  exports:[
+    PaginationModule,
+    PagingHeaderComponent
   ]
 })
 export class SharedModule { }
